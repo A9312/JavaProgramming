2523b86235372c6183f3206ce6ec3452b17f91c9
akshit <akshit@Akshit>
2026-06-19 02:19:08 +0530

    Second Commit

---
+++ JavaProgramming/src/day3/OperatorsDemo.java
@@ -0,0 +1,33 @@
+package day3;
+
+public class OperatorsDemo {
+
+	public static void main(String[] args)
+	{
+		int a=10;
+		
+		a++; 		//a=a+1;
+		
+		System.out.println(a);
+		
+		int b=10;
+		
+//		int res=b++;
+		
+//		System.out.println(res);
+		
+		int c=10;
+		
+		int res=++c;
+		
+		System.out.println(res);
+
+		int d = 10;
+		
+		d+=5;
+		
+		System.out.println(d);
+		
+		
+	}
+}
---
+++ JavaProgramming/src/day3/TernaryOperatory.java
@@ -0,0 +1,14 @@
+package day3;
+
+public class TernaryOperatory {
+	
+	public static void main(String[] args)
+	{
+		int a=100, b=200;
+		
+		int x=(a>b)?a:b;
+		
+		System.out.println(x);
+	}
+
+}
